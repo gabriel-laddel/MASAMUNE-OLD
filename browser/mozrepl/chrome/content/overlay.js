1652cc452041f879db14e15ff81b490a40494bc4
Gabriel Laddel <gabrielvalethladdel@gmail.com>
2015-04-24 19:10:16 +0000

    deleted a bunch of browser related things

--- browser/mozrepl/chrome/content/overlay.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener(
-    'load', function(event) { mozrepl.initOverlay(); }, false);
-
-var mozrepl = {};
-
-Components
-.classes['@mozilla.org/moz/jssubscript-loader;1']
-.getService(Components.interfaces.mozIJSSubScriptLoader)
-    .loadSubScript('chrome://mozrepl/content/overlay_impl.js', mozrepl);
