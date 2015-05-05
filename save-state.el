dc7f64f7d44aafc769883b6e5d1546e25bf7ff55
Gabriel Laddel <gabrielvalethladdel@gmail.com>
2015-05-05 14:07:01 +0000

    suck it!

--- save-state.el
+++ save-state.el
@@ -1,5 +1,9 @@
-(defun revive:window-edges (&optional window)
-  (window-edges window))
+;; (current-window-configuration-printable)
+
+;; (restore-window-configuration
+;;  '(239 79 ((0 0 119 78) (119 0 239 78)) 
+;;        (("~/quicklisp/local-projects/masamune/save-state.el" "save-state.el" 396 1)
+;; 	("~/documents/A_Mathematicians_s_Apology.pdf" "A_Mathematicians_s_Apology.pdf" 5 1))))
 
 (cond
  ((fboundp 'screen-height)
@@ -9,6 +13,27 @@
   (fset 'revive:screen-height 'frame-height)
   (fset 'revive:screen-width 'frame-width))
  (t (error "I don't know how to run revive.el on this Emacs...")))
+
+(defun pdf-filename? (filename)
+  (string= "fdp." (substring (string/reverse filename) 0 4)))
+
+(defun abort-if-file-too-large (size op-type filename)
+  "If file SIZE larger than `large-file-warning-threshold', allow user to abort.
+OP-TYPE specifies the file operation being performed (for message to user)."
+  ;; NOTE 2015-05-04T10:40:59+00:00 Gabriel Laddel
+  ;; We assume that you always want to open large .pdfs, because otherwise
+  ;; `restore-doc-page' won't work.
+  (unless (and (stringp filename) (pdf-filename? filename))
+    ;; (progn (add-hook 'doc-view-mode-hook #'restore-doc-page) nil)
+    (when (and large-file-warning-threshold size
+  	       (> size large-file-warning-threshold)
+  	       (not (y-or-n-p (format "File %s is large (%s), really %s? "
+  				      (file-name-nondirectory filename)
+  				      (file-size-human-readable size) op-type))))
+      (error "Aborted"))))
+
+(defun revive:window-edges (&optional window)
+  (window-edges window))
 
 (defun revive:minx () 0)
 (defun revive:miny ()
@@ -30,27 +55,6 @@
     (while (not (eq curwin (setq win (next-window win "w/o mini"))))
       (setq wlist (append wlist (list win)))) ;use append to preserve order
     wlist))
-
-(defun revive:window-buffer-list ()
-  "Return the all shown buffer list.
-Each element consists of '(buffer-file-name window-start point)"
-  (let ((curw (selected-window))(wlist (revive:window-list)) wblist)
-    (save-excursion
-      (while wlist
-	(select-window (car wlist))
-	(set-buffer (window-buffer (car wlist))) ;for Emacs 19
-	(setq wblist
-	      (append wblist
-		      (list (list
-			     (if (and (fboundp 'abbreviate-file-name)
-				      (buffer-file-name))
-				 (abbreviate-file-name (buffer-file-name))
-			       (buffer-file-name))
-			     (window-start)
-			     (point))))
-	      wlist (cdr wlist)))
-      (select-window curw)
-      wblist)))
 
 (defun revive:all-window-edges ()
   "Return the all windows edges by list."
@@ -201,7 +205,9 @@ property lists are stored in order corresponding to Edge-List.  Buffer
 property list is formed as
 '((buffer-file-name) (buffer-name) (point) (window-start))."
   (let ((curwin (selected-window))
-	(wlist (revive:window-list)) (edges (revive:all-window-edges)) buflist)
+	(wlist (revive:window-list)) 
+	(edges (revive:all-window-edges)) 
+	buflist)
     (save-excursion
       (while wlist
 	(select-window (car wlist))
@@ -216,20 +222,20 @@ property list is formed as
 					  (buffer-file-name))
 				       (buffer-file-name))
 				     (buffer-name)
-				     (point)
+				     (if (eq 'doc-view-mode major-mode)
+					 (doc-view-current-page) 
+				       (point))
 				     (window-start))))
 	      wlist (cdr wlist)))
       (select-window curwin)
       (list (revive:screen-width) (revive:screen-height) edges buflist))))
 
-(defmacro revive:get-file (x)
-  (list 'car x))
-(defmacro revive:get-buffer (x)
-  (list 'nth 1 x))
-(defmacro revive:get-point (x)
-  (list 'nth 2 x))
-(defmacro revive:get-window-start (x)
-  (list 'nth 3 x))
+(defun revive:get-buffer (l)
+  (nth 1 l))
+(defun revive:get-point (l)
+  (nth 2 l))
+(defun revive:get-window-start (l)
+  (nth 3 l))
 
 (defun revive:find-file (file)
   "Make the best effort to find-file FILE."
@@ -248,195 +254,73 @@ property list is formed as
 	       (current-buffer))))
    (t nil)))
 
+(defvar page-to-jump nil "")
+(defvar doc-buffer nil "")
+
+(defun restore-doc-page ()
+  (message "restore doc page, %S, %S" doc-buffer page-to-jump)
+  (cond ((null doc-buffer))
+	((buffer-around? doc-buffer) (progn (switch-to-buffer doc-buffer)
+					    (doc-view-goto-page page-to-jump)
+					    (remove-hook 'doc-view-mode-hook #'restore-doc-page)
+					    (setq page-to-jump nil doc-buffer nil)))
+	(t (error "couldn't find doc-buffer"))))
+
 (defun restore-window-configuration (config)
   "Restore the window configuration.
 Configuration CONFIG should be created by
 current-window-configuration-printable."
-  (let ((width (car config)) (height (nth 1 config))
-	(edges (nth 2 config)) (buflist (nth 3 config)) buf)
+  (let* ((width (car config)) 
+	 (height (nth 1 config))
+	 (edges (nth 2 config))
+	 (buffers (nth 3 config)))
     (set-buffer (get-buffer-create "*scratch*"))
     (setq edges (revive:normalize-edges width height edges))
     (construct-window-configuration edges)
     (revive:select-window-by-edge (revive:minx) (revive:miny))
-    (while buflist
-      (setq buf (car buflist))
-      (cond
-       ((and (revive:get-buffer buf)
-	     (get-buffer (revive:get-buffer buf)))
-	(switch-to-buffer (revive:get-buffer buf))
-	(goto-char (revive:get-window-start buf)) ;to prevent high-bit missing
-	(set-window-start nil (point))
-	(goto-char (revive:get-point buf)))
-       ((and (stringp (revive:get-file buf))
-	     (not (file-directory-p (revive:get-file buf)))
-	     (revive:find-file (revive:get-file buf)))
-	(set-window-start nil (revive:get-window-start buf))
-	(goto-char (revive:get-point buf))))
-      (setq buflist (cdr buflist))
-      (other-window 1))))
+    (loop for buffer-description in buffers
+	  do (let* ((maybe-buffer (nth 1 buffer-description))
+		    (maybe-buffer (aif (and maybe-buffer (get-buffer maybe-buffer))
+				      it maybe-buffer))
+		    (maybe-file (car buffer-description)))	       
+	       (cond ((and maybe-buffer (stringp maybe-file) (pdf-filename? maybe-file))
+		      (message "1. maybe-file: %S maybe-buffer %S" maybe-file maybe-buffer)
+		      (aif (buffer-with-filename maybe-file)
+			  (progn (switch-to-buffer it)
+				 (goto-char (revive:get-window-start buffer-description)) ;to prevent high-bit missing
+				 (set-window-start nil (point))
+				 (doc-view-goto-page (or (revive:get-point buffer-description) 1))
+				 (message "um.... really?"))
+			(progn (setf page-to-jump (or (revive:get-point buffer-description) 1)
+				     doc-buffer maybe-buffer)
+			       (add-hook 'doc-view-mode-hook #'restore-doc-page)
+			       (find-file maybe-file)
+			       (message "yeah, I guess"))))
+		     
+		     (maybe-buffer 
+		      (message "2. maybe-file: %S maybe-buffer %S" maybe-file maybe-buffer)
+		      (switch-to-buffer maybe-buffer)
+		      (goto-char (revive:get-window-start buffer-description)) ;to prevent high-bit missing
+		      (set-window-start nil (point))
+		      (goto-char (revive:get-point buffer-description)))
+ 
+		     ((and (stringp maybe-file)
+			   (not (file-directory-p maybe-file))
+			   (revive:find-file maybe-file))
+		      (message "3. maybe-file: %S maybe-buffer %S" maybe-file maybe-buffer)
+		      (let* ((window-start (nth 3 buffer-description))
+			     (buffer-description-point (nth 2 buffer-description)))
+			(set-window-start nil window-start)
+			(if (and (stringp maybe-file) (pdf-filename? maybe-file)) 
+			    (message "handle pdf in second `cond' clause")
+			  (goto-char buffer-description-point)))))
+	       (other-window 1)))))
 
-(defun revive:buffer-list ()
-  (delq nil (buffer-list)))
-
-(defun revive:map-string-match (string alist)
-  "Return sub-list if a car component of list in ALIST matches STRING.
-That car component is regexp."
-  (let (regex)
-    (catch 'match
-      (while alist
-	(setq regex (car (car alist)))
-	(if (and regex
-		 (stringp regex)
-		 (string-match (car (car alist)) string))
-	    (throw 'match t))
-	(setq alist (cdr alist))))))
-
-(defun revive:buffer-property-list ()
-  "Return buffers property list.
-Returned list is a form of: '(Buffer-List Variable-List).
-Buffer-List is a list as
-'((buffer-file-name) (buffer-name) major-mode (point) (mark)).
-Variable-List is a return value of revive:varlist."
-  (let ((buflist (revive:buffer-list)) plist
-	(local-var (append revive:save-variables-local-default
-			   revive:save-variables-local-private))
-	(mode-local-var (append revive:save-variables-mode-local-default
-				revive:save-variables-mode-local-private)))
-    (save-excursion
-      (while buflist
-	(set-buffer (car buflist))
-	(if (or (assq major-mode revive:major-mode-command-alist)
-		(revive:map-string-match
-		 (buffer-name) revive:major-mode-command-alist)
-		(if (and (fboundp 'abbreviate-file-name) (buffer-file-name))
-		    (abbreviate-file-name (buffer-file-name))
-		  (buffer-file-name)))
-	    (setq plist
-		  (append
-		   plist
-		   (list
-		    (list
-		     (if (and (fboundp 'abbreviate-file-name)
-			      (buffer-file-name))
-			 (abbreviate-file-name (buffer-file-name))
-		       (buffer-file-name))
-		     (buffer-name)
-		     major-mode
-		     (point)
-		     (mark t)
-		     nil)))))
-	(setq buflist (cdr buflist))))
-    (list plist nil)))
-
-(defmacro revive:prop-file-name (x)
-  (list 'car x))
-(defmacro revive:prop-buffer-name (x)
-  (list 'nth 1 x))
-(defmacro revive:prop-major-mode (x)
-  (list 'nth 2 x))
-(defmacro revive:prop-point (x)
-  (list 'nth 3 x))
-(defmacro revive:prop-mark (x)
-  (list 'nth 4 x))
-(defmacro revive:prop-varlist (x)
-  (list 'nth 5 x))
-(defmacro revive:prop-get-value (x y)
-  (list 'cdr (list 'assq y (list 'nth 5 x))))
-
-(defun save-current-configuration (&optional num)
-  "Save current window/buffer configuration into configuration file."
-  (interactive "p")
-  (let ((bufs (revive:buffer-property-list))
-	(config (current-window-configuration-printable)))
-    (set-buffer
-     (find-file-noselect (expand-file-name revive:configuration-file)))
-    (emacs-lisp-mode)
-    (widen)
-    (goto-char (point-min))
-    (and (search-forward revive:version-prefix nil t)
-	 (goto-char (match-beginning 0))
-	 (delete-region (point) (progn (forward-line 1) (point))))
-    (insert (format "%s%s\n" revive:version-prefix revive:version))
-    (setq num (or num 1))
-    (if (re-search-forward (format "^(%d" num) nil t)
-	(progn (goto-char (match-beginning 0))
-	       (delete-region (point) (progn (forward-list 1)(point)))
-	       (delete-char 1))
-      (goto-char (point-max)))
-    (delete-blank-lines)
-    (if (not (bolp)) (newline 1))
-    (insert (format "%s\n" (prin1-to-string (list num bufs config))))
-    (basic-save-buffer)
-    (kill-buffer (current-buffer))))
-
-(defun revive:restore-value (vlist)
-  "Restore variables in VLIST which is a return value of revive:varlist."
-  (while vlist
-    (if (and (car vlist) (listp (car vlist)))
-	(set (car (car vlist)) (cdr (car vlist))))
-    (setq vlist (cdr vlist))))
-
-(defun revive:restore-buffers (buflist)
-  "Restore all buffers in BUFLIST which is from revive:buffer-property-list."
-  (let ((blist (car buflist)) x command success
-	(mmc-alist revive:major-mode-command-alist))
-    (revive:restore-value (car (cdr buflist)))
-    (while blist
-      (setq x (car blist) success nil)
-      (if (setq command (or (assq (revive:prop-major-mode x) mmc-alist)
-			    (assoc (revive:prop-buffer-name x) mmc-alist)))
-	  (condition-case err
-	      (let ((noninteractive nil))
-		(if (fboundp (cdr command))
-		    (progn
-		      (call-interactively (cdr command))
-		      (setq success t))))
-	    ;;(funcall (cdr command))
-	    (error (message "%s: %s." (cdr command) err) (sit-for 1)))
-	(if (revive:find-file (revive:prop-file-name x))
-	    (progn
-	      (if (and (not (eq (revive:prop-major-mode x) major-mode))
-		       (fboundp (revive:prop-major-mode x)))
-		  (if (commandp (revive:prop-major-mode x))
-		      (call-interactively (revive:prop-major-mode x))
-		    (funcall (revive:prop-major-mode x))))
-	      (setq success t)
-	      )))
-      (cond
-       (success
-	(and (not (string= (revive:prop-buffer-name x) (buffer-name)))
-	     (not  (get-buffer (revive:prop-buffer-name x)))
-	     (rename-buffer (revive:prop-buffer-name x)))
-	(set-mark (revive:prop-mark x))
-	(goto-char (revive:prop-point x))
-	(and (fboundp 'region-active-p) (region-active-p) (deactivate-mark))
-	(revive:restore-value (revive:prop-varlist x))))
-      (setq blist (cdr blist)))))
-
-(defun resume (&optional num)
-  "Resume window/buffer configuration.
-Configuration should be saved by save-current-configuration."
-  (interactive "p")
-  (setq num (or num 1))
-  (let (sexp bufs x config)
-    (find-file (expand-file-name revive:configuration-file))
-    (goto-char (point-min))
-    (emacs-lisp-mode)
-    (if (null (search-forward revive:version-prefix nil t))
-	(error "Configuration file collapsed."))
-    (if (and (not (string= revive:version
-			   (buffer-substring
-			    (point)
-			    (prog2 (end-of-line) (point)))))
-	     (not (y-or-n-p
-		   "Configuration file's version conflicts. Continue?")))
-	(error "Configuration file is old.  Please update."))
-    (if (null (re-search-forward (format "^(%d" num) nil t))
-	(error "Configuration empty."))
-    (goto-char (match-beginning 0))
-    (setq sexp (read (current-buffer))
-	  bufs (nth 1 sexp)
-	  config (nth 2 sexp))
-    (kill-buffer (current-buffer))
-    (revive:restore-buffers bufs)
-    (restore-window-configuration config)))
+(defun update-emacs-and-browser-state ()
+  ;; XXX 2015-05-03T00:26:34+00:00 Gabriel Laddel
+  ;; - trivial timers were not working as reliably as emacs timers, hence.
+  ;; - this function assumes that mm::*swank-connection-hack* has been bound to
+  ;; - standard outuput from the buffer *slime-repl sbcl*
+  (slime-eval-async `(cl::progn (cl::setf (cl::getf mm::state :emacs) 
+					  (quote ,(current-window-configuration-printable)))
+				(mmb::update-browser-state))))
